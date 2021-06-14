082b47a2ace533696141d1594837aaaf8652df91
Rob Kuijlenburg <robkuijlenburg91@gmail.com>
2021-06-14 11:22:19 +0200

    html en js ingevoerd

--- scripts.js
+++ scripts.js
@@ -1,1 +1,29 @@
+function gooi() {
+    const dobbelArray = [];
+    for (i = 0; i <= 7; i++) {
+        let randomNum = (Math.floor(Math.random() * 6)) + 1;
+        dobbelArray.push(randomNum);
+    }
+    console.log(dobbelArray);
+    dobbelArray.sort();
+    const worp = [];
 
+    // (worp[x] || 0]) keert de waarde van worp[x] terug anders 0
+    // daarna 1 toevoegen terugkeren naar de array en naar de volgende
+    dobbelArray.forEach(function (x) {
+        worp[x] = (worp[x] || 0) + 1;
+    });
+    console.log(worp);
+
+
+    // zoekt voor 6 cijfers de geworpen hoeveelheden en keert 0 terug wanneer niet gedefinieerd
+    for (y = 1; y <= 6; y++) {
+        const tdSelect = 'td' + y;
+        if (worp[y] === undefined) {
+            document.getElementById(tdSelect).innerHTML = '0';
+    
+        } else {
+            document.getElementById(tdSelect).innerHTML = worp[y];
+        }
+    }
+}
