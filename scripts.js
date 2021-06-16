f42a4564fc52c9f80247f4778d50bc0576edcf70
Gerard Oosterhof <gerard@script.nl>
2021-06-16 11:43:07 +0200

    Code Review

--- scripts.js
+++ scripts.js
@@ -8,6 +8,9 @@ function gooi() {
     dobbelArray.sort();
     const worp = [];
 
+    // TODO :: deze foreach is overbodig, je kan wat er in gebeurt ook meteen toepassen in de loop hierboven
+    // En dat maakt dobbelArray dan ook meteen weer overbodig
+    
     // (worp[x] || 0]) keert de waarde van worp[x] terug anders 0
     dobbelArray.forEach(function (x) {
         worp[x] = (worp[x] || 0) + 1;
@@ -18,6 +21,7 @@ function gooi() {
     // zoekt voor 6 cijfers de geworpen hoeveelheden en keert 0 terug wanneer niet gedefinieerd
     for (y = 1; y <= 6; y++) {
         const tdSelect = 'td' + y;
+        // TODO :: gebruik hier dezelfde tactiek als je hierboven bij de forEach doet.
         if (worp[y] === undefined) {
             document.getElementById(tdSelect).innerHTML = '0';
     
